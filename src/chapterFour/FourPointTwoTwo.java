157b9911050840ba37380667a7dfecf1846ea8cd
forverchild <dabreezy8@gmail.com>
2021-10-11 22:14:54 +0100

    today

---
+++ src/chapterFour/FourPointTwoTwo.java
@@ -0,0 +1,11 @@
+package chapterFour;
+
+public class FourPointTwoTwo {
+    public static void main(String[] args) {
+        System.out.println("N\t 10*N\t 100*N\t 1000*N\t");
+
+        for (int i = 1; i <=5 ; i++) {
+            System.out.println(i + "\t "+10*i+"\t\t "+ 100*i+ "\t "+ 1000*i);
+        }
+    }
+}
