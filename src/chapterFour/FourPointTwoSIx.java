fffa121a0b12bc28189aeb8f6ffd2b358a7bba9b
forverchild <dabreezy8@gmail.com>
2021-10-14 21:38:10 +0100

    point two six

---
+++ src/chapterFour/FourPointTwoSIx.java
@@ -0,0 +1,20 @@
+package chapterFour;
+
+public class FourPointTwoSIx {
+        public static void main(String[] args)
+        {
+            int row = 10;
+            while (row >= 1)
+            {
+                int column = 1;
+                while (column <= 10)
+                {
+                    System.out.print(row % 2 == 1 ? "<" : ">");
+                    ++column;
+                }
+                --row;
+                System.out.println();
+            }
+        }
+
+}
