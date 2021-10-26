f4835a760203ff49d78ecdd4b2c9b662d2644577
forverchild <dabreezy8@gmail.com>
2021-10-26 16:20:22 +0100

    chapter four

---
+++ src/chapterFour/FourPointThreeTwo.java
@@ -0,0 +1,12 @@
+package chapterFour;
+
+public class FourPointThreeTwo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.print("* ".repeat(8));
+            System.out.print(" ");
+            System.out.println();
+        }
+
+    }
+}
